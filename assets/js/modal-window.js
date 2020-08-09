7fab697ffbb8a65f8e1d35128cdd3ad71f3de3e0
ameliabultitude <millybultitude_@hotmail.co.uk>
2020-08-09 18:02:00 +0000

    putting in a working un styled modal box

--- assets/js/modal-window.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var $content = $('#share-options').detach();
-
-    $('#share').on('click', function() {
-        MSFIDOCredentialAssertion.open({content: $content, width:340, height:300});
-    });
-});
