20f6245321693d6dcdc2a118d9efef5a5a835904
ameliabultitude <millybultitude_@hotmail.co.uk>
2020-08-17 19:26:34 +0000

    Adding in comments and beautifying code

--- assets/js/btspvc.js
+++ assets/js/btspvc.js
@@ -1,11 +1,11 @@
-      $(function() {
-        $('#exampleModal').on('show.bs.modal', function(event) {
-          var button = $(event.relatedTarget);
-          var recipient = button.data('id');
-          var content = button.data('content');
-          var modal = $(this);
-          modal.find('.modal-title').text('' + recipient);
-          modal.find('.modal-body input').val(recipient);
-          modal.find('.modal-body').text(content);
-        });
-      });
+$(function() {
+    $('#exampleModal').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget);
+        var recipient = button.data('id');
+        var content = button.data('content');
+        var modal = $(this);
+        modal.find('.modal-title').text('' + recipient);
+        modal.find('.modal-body input').val(recipient);
+        modal.find('.modal-body').text(content);
+    });
+});
