924425c8f0aecd7168e9e64d176d646dbbddfeaa
ameliabultitude <millybultitude_@hotmail.co.uk>
2020-08-20 18:59:56 +0000

    Edit: Making modals responsive in blogs

--- assets/js/readMoreModal.js
+++ assets/js/readMoreModal.js
@@ -1,6 +1,7 @@
 // JS opening modal function to opening the modal so readers can read more of the text.
 $(function() {
     $('#exampleModal').on('show.bs.modal', function(event) {
+        $('#exampleModal').addClass('zfighter');
         var button = $(event.relatedTarget);
         var recipient = button.data('id');
         var content = button.data('content');
